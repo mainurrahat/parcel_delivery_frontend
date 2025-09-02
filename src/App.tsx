5ab632514098f880f2d20b5562d62c09a756c4cb
mainurrahat <mainurrahat51@gmail.com>
2025-09-02 23:28:30 +0600

    navber footer size ok

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,18 @@
-// App.tsx
 import { Outlet } from "react-router-dom";
-import Footer from "./layouts/Footer";
 import Navbar from "./layouts/Navbar";
+import Footer from "./layouts/Footer";
 
 function App() {
   return (
     <div className="flex flex-col min-h-screen">
       {/* Navbar */}
       <Navbar />
-      {/* Main Content */}
-      <Outlet />
+
+      {/* Main content */}
+      <main className="flex-grow container mx-auto px-4 py-6">
+        <Outlet />
+      </main>
+
       {/* Footer */}
       <Footer />
     </div>
